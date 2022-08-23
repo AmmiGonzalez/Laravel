a008558c138f3cf12996bcade95352271b1acf8f
Ammi González <64441015+AmmiGonzalez@users.noreply.github.com>
2022-08-23 13:40:51 -0600

    Actualización de proyecto

--- resources/views/dashboards/posts/create.blade.php
+++ resources/views/dashboards/posts/create.blade.php
@@ -10,6 +10,20 @@
     <h1>Bienvenidos</h1>
 <form action="{{route('post.store')}}" method = "post">
         @csrf
+
+        <label for="">Título</label>
+        <input type="text" name="title">
+
+        <label for="">URL Corta</label>
+        <input type="text" name="slug">
+
+        <label for="">Contenido</label>
+        <!-- El input es un textbox mientras que el textarea brinda un espacio para textos largos. -->
+        <textarea name="content"></textarea>
+
+        <label for="">Descripción</label>
+        <textarea name="description"></textarea>
+
     <button type = "submit">Enviar</button>
     </form>
 </body>
